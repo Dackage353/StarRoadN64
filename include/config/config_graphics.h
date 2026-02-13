6200b39007a7e9972f32dc2da5fe669e3119b36b
aglab2 <aglab3@gmail.com>
2026-02-13 23:29:26 +0800

    Fixed pool overflow in CCCoral

--- include/config/config_graphics.h
+++ include/config/config_graphics.h
@@ -7,7 +7,7 @@
 /**
  * The size of the master display list (gDisplayListHead). 6400 is vanilla.
  */
-#define GFX_POOL_SIZE 11000
+#define GFX_POOL_SIZE 12000
 
 /**
  * Causes the global light direction to be in world space,
