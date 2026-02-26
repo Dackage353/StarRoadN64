72a7abe3492f1b6d6d7c3d284ae9e6950e7fea53
aglab2 <aglab3@gmail.com>
2026-02-26 21:32:40 +0800

    Do not randomize MC+Shell in FFF

--- src/engine/behavior_script.c
+++ src/engine/behavior_script.c
@@ -357,6 +357,10 @@ static s32 bhv_cmd_randomize_object(void) {
     {
         goto end;
     }
+    if (gCurrLevelNum == LEVEL_THI && gCurrentObject->behavior == bhvExclamationBox && (gCurrentObject->oBehParams2ndByte == 3 || gCurrentObject->oBehParams2ndByte == 1))
+    {
+        goto end;
+    }
 
     u32 randType = BHV_CMD_GET_U32(1);
     u8 randomize = FALSE;
