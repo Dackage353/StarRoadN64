e11f8193385e03c4dd42e3d99eadf709e7224d28
aglab2 <aglab3@gmail.com>
2026-02-27 20:44:31 +0800

    Do not randomize MC+VC in SLR

--- src/engine/behavior_script.c
+++ src/engine/behavior_script.c
@@ -361,6 +361,10 @@ static s32 bhv_cmd_randomize_object(void) {
     {
         goto end;
     }
+    if (gCurrLevelNum == LEVEL_WF && gCurrentObject->behavior == bhvExclamationBox && (gCurrentObject->oBehParams2ndByte <= 2))
+    {
+        goto end;
+    }
 
     u32 randType = BHV_CMD_GET_U32(1);
     u8 randomize = FALSE;
