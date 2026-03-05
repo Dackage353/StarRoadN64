fdeb67b0441c2414e49d2f7dbcda65a19eed99ef
aglab2 <aglab3@gmail.com>
2026-03-05 21:54:10 +0800

    Added B1F and increased CCCandy hitbox radius

--- src/game/randomizer.c
+++ src/game/randomizer.c
@@ -21,7 +21,7 @@
 #include "segment2.h"
 #include "game/emutest.h"
 
-u32 Randomizer_gGameSeed = 3744382;
+u32 Randomizer_gGameSeed = 2976852;
 
 u8 Randomizer_gIsSetSeed = FALSE;
 
--- src/game/randomizer_data.h
+++ src/game/randomizer_data.h
@@ -128,10 +128,14 @@ static const struct Randomizer_AvoidancePoint ccAvoidancePoints[] = {
 };
 
 static const struct Randomizer_AvoidancePoint ccandyAvoidancePoints[] = {
-    {bhvStub, {580, 2003, 5703}, 1500.0f, 1500.0f, Randomizer_AVOIDANCE_SAFETY_HARD}, // target
+    {bhvStub, {580, 1803, 5703}, 1500.0f, 1700.0f, Randomizer_AVOIDANCE_SAFETY_HARD}, // target
     {bhvStub, {-4527, -3297, 1661}, 1100.0f, 500.0f, Randomizer_AVOIDANCE_SAFETY_HARD}, // top of the coin
     {bhvStub, {-5134, 890, -1687}, 1000.0f, 1000.0f, Randomizer_AVOIDANCE_SAFETY_HARD}, // on roof for coin
     {bhvStub, {-5134, 890, -1687}, 1000.0f, 1000.0f, Randomizer_AVOIDANCE_SAFETY_HARD}, // on roof for coin
+};
+
+static const struct Randomizer_AvoidancePoint b1fAvoidancePoints[] = {
+    {bhvStub, {3258, 465, -2816}, 700.0f, 700.0f, Randomizer_AVOIDANCE_SAFETY_ALL} // top of the roof
 };
 
 
@@ -225,7 +229,7 @@ static const struct Randomizer_AreaParams icParams[] = {
 };
 
 static const struct Randomizer_AreaParams b1fParams[] = {
-    {-5149, 5180, -1006, 434, -5619, 5745, 0, 0, NULL}
+    {-5149, 5180, -1006, 434, -5619, 5745, 0, ARRAY_SIZE(b1fAvoidancePoints), &b1fAvoidancePoints}
 };
 static const struct Randomizer_AreaParams b2fParams[] = {
     {-5149, 5180, -1006, 434, -5619, 5745, 0, 0, NULL}
